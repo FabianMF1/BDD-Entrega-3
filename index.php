f05c72deb33827e6b3a43b09b0b9aed7b1b4a9df
RadiosMark <dpgarces@uc.cl>
2023-06-30 02:03:00 -0400

    tenemos redirecciones

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
   <h2 class = "subtitle"  align="center">Iniciar Sesión</h2>
 
   <form align="center" method="post">
-    <input type="text" name="new_client_name" placeholder="Nombre de Usuario">
-    <input type="password" name="new_client_password" placeholder="Contraseña">
+    <input type="text" name="user_name" placeholder="Nombre de Usuario">
+    <br/><br/>
+    <input type="password" name="user_password" placeholder="Contraseña">
     <br/><br/>
     <input type="submit" name = 'log_in' value="Iniciar Sesión"> 
     <br/><br/>
     <input type="submit" name = 'import' value="Importar Usuarios">
   </form>
 
-  <?php
-  include('validation.php')
-  ?>
   <br>
   <br>
 
   <a href="guest/menu.php">Continúa como invitado</a>
+  <br>
+
+  <?php
+  include('validation.php')
+  ?>
 
 </body>
--- validation.php
+++ validation.php
@@ -2,25 +2,53 @@
 # no tocar estoy arreglandolo (Diego)
 
 # accede a los PDO y obtiene las variables de db
-include('../config/conexion.php');
-include('../functions/rdm_str.php');
+include('config/conexion.php');
+include('functions/rdm_str.php');
 
-$password = generate_string($perm_chars, 10);
 
 if (isset($_POST['import'])){
+    # seleccionamos a todos los clientes de la tabla
     $query = "SELECT * FROM cliente ORDER BY id_cliente;";
-    $result = $db1-> prepare($query);
+    $result = $db1 -> prepare($query);
     $result -> execute();
     $clientes = $result -> fetchAll();
-
+    #por cada uno
     foreach ($clientes as $cliente){
         $password = generate_string($perm_chars, 10); #generamos una contraseña aleatoria
-        $query = "SELECT user_client($cliente[0], '$cliente[1]'::varchar, '$password'::varchar);";
+        $query = "SELECT user_client('$cliente[0]', '$cliente[1]', '$password');";
 
-        // Ejecutamos las querys para efectivamente insertar los datos
         $result = $db1 -> prepare($query);
         $result -> execute();
         $result -> fetchAll();
+    }
+}
+
+if (isset($_POST['log_in'])){
+    $user_name = $_POST['user_name'];
+    $user_password = $_POST['user_password'];
+    $query = "SELECT * FROM usuario WHERE nombre = '$user_name' AND contrasena='$user_password';";
+    $result = $db1 -> prepare($query);
+    $result -> execute();
+    $usuario = $result -> fetchAll();
+    if ($usuario){ #si la consulta no es vacia, se trata de un usuario en la base de datos
+        $id_u = $usuario[0][0];
+        $name_u = $usuario[0][1];
+        $type_u = $usuario[0][2];
+        $password_u = $usuario[0][3];
+        if ($type_u == 'admin'){ #redireccionamos al menu de administrador
+            
+            header('Location: admin/menu.php');
+            exit();
+
+        }
+        else{ #redireccionamos al menu de cliente
+            header('Location: client/menu.php');
+            exit();
+        }
+
+    }
+    else{
+        echo "<h2 class='subtitle'> ¡Usuario o Contraseña Incorrectos!  </h2>";
     }
 }
 
